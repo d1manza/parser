737465edced9c216d0894ffb3f4de6d7e4ac23a5
kosolapov_d <kosolapov.d@orionnet.ru>
2023-06-30 13:29:16 +0700

    refactoring

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const {setIntervalAsync} = require('set-interval-async');
 const config = require('./app/config/config');
 const Start = require('./app/services/start');
 const start = new Start();
@@ -13,9 +12,10 @@ async function run() {
         await shared.sleep(config.settings.intervalFailedLaunch * 1000 * 60);
         await run();
     } else {
-        await shared.logging('start', 'successfully', `Restart affter ${config.settings.intervalFailedLaunch} minutes`);
+        await shared.logging('start', 'successfully', `Restart affter ${config.settings.intervalSuccessfulLaunch} minutes`);
         await shared.sleep(config.settings.intervalSuccessfulLaunch * 1000 * 60);
         await run();
+        await shared.logging('start', 'successfully', `End parsing + mailing`);
     }
 }
 
